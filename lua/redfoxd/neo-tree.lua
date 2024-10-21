4270692ec23c37a84e6b1dacb4b4ea4f9ee164d1
ricardoprato <ricardoprato36@gmail.com>
2024-10-20 22:11:54 -0300

    📷 [ADD] image render support and new functions

--- lua/redfoxd/neo-tree.lua
+++ lua/redfoxd/neo-tree.lua
@@ -5,7 +5,22 @@ return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
   dependencies = {
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    { 'nvim-tree/nvim-web-devicons' }, -- not strictly required, but recommended
+    {
+      '3rd/image.nvim',
+      dependencies = {
+        'leafo/magick',
+        {
+          'vhyrro/luarocks.nvim',
+          opts = {
+            rocks = {
+              hererocks = true,
+            },
+          },
+        },
+      },
+      opts = {}
+    }, -- image support in preview windo
   },
   cmd = 'Neotree',
   keys = {
@@ -14,42 +29,46 @@ return {
   opts = {
     commands = {
       copy_selector = function(state)
-          local node = state.tree:get_node()
-          local filepath = node:get_id()
-          local filename = node.name
-          local modify = vim.fn.fnamemodify
+        local node = state.tree:get_node()
+        local filepath = node:get_id()
+        local filename = node.name
+        local modify = vim.fn.fnamemodify
 
-          local vals = {
-            ["BASENAME"] = modify(filename, ":r"),
-            ["EXTENSION"] = modify(filename, ":e"),
-            ["FILENAME"] = filename,
-            ["PATH (CWD)"] = modify(filepath, ":."),
-            ["PATH (HOME)"] = modify(filepath, ":~"),
-            ["PATH"] = filepath,
-            ["URI"] = vim.uri_from_fname(filepath),
-          }
+        local vals = {
+          ['BASENAME'] = modify(filename, ':r'),
+          ['EXTENSION'] = modify(filename, ':e'),
+          ['FILENAME'] = filename,
+          ['PATH (CWD)'] = modify(filepath, ':.'),
+          ['PATH (HOME)'] = modify(filepath, ':~'),
+          ['PATH'] = filepath,
+          ['URI'] = vim.uri_from_fname(filepath),
+        }
 
-          local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
-          if vim.tbl_isempty(options) then
-            return
+        local options = vim.tbl_filter(function(val)
+          return vals[val] ~= ''
+        end, vim.tbl_keys(vals))
+        if vim.tbl_isempty(options) then
+          return
+        end
+        table.sort(options)
+        vim.ui.select(options, {
+          prompt = 'Choose to copy to clipboard:',
+          format_item = function(item)
+            return ('%s: %s'):format(item, vals[item])
+          end,
+        }, function(choice)
+          local result = vals[choice]
+          if result then
+            vim.fn.setreg('+', result)
           end
-          table.sort(options)
-          vim.ui.select(options, {
-            prompt = "Choose to copy to clipboard:",
-            format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
-          }, function(choice)
-            local result = vals[choice]
-            if result then
-              vim.fn.setreg("+", result)
-            end
-          end)
+        end)
       end,
       find_in_dir = function(state)
-          local node = state.tree:get_node()
-          local path = node:get_id()
-          require("telescope.builtin").live_grep {
-            cwd = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h"),
-          }
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require('telescope.builtin').live_grep {
+          cwd = node.type == 'directory' and path or vim.fn.fnamemodify(path, ':h'),
+        }
       end,
     }, -- A list of functions
     filesystem = {
