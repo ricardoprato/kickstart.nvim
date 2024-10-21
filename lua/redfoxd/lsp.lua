b1562f15a457b705fe69df0f3c5c826e2c080693
ricardoprato <ricardoprato36@gmail.com>
2024-10-20 22:09:27 -0300

    ⬆️ [UPD] better key mapping for LSP functions

--- lua/redfoxd/lsp.lua
+++ lua/redfoxd/lsp.lua
@@ -53,19 +53,19 @@ return { -- LSP Configuration & Plugins
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+        map('<leader>ld', require('telescope.builtin').lsp_document_symbols, '[D]ocument Symbols')
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map('<leader>lw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace Symbols')
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        map('<leader>lr', vim.lsp.buf.rename, '[R]ename')
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        map('<leader>lc', vim.lsp.buf.code_action, '[C]ode Action')
 
         -- Opens a popup that displays documentation about the word under your cursor
         --  See `:help K` for why this keymap.
@@ -146,7 +146,7 @@ return { -- LSP Configuration & Plugins
       -- tsserver = {},
       --
       deno = {},
-
+      html = {},
       lua_ls = {
         -- cmd = {...},
         -- filetypes = { ...},
