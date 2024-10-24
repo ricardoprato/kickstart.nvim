8c682888ac4275406e665cab325eb0c5abbb33ef
ricardoprato <ricardoprato36@gmail.com>
2024-10-24 08:16:10 -0300

    ✨ [ADD] capability validations

--- lua/redfoxd/lsp.lua
+++ lua/redfoxd/lsp.lua
@@ -25,6 +25,7 @@ return { -- LSP Configuration & Plugins
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
         -- NOTE: Remember that Lua is a real programming language, and as such it is possible
         -- to define small helper and utility functions so you don't have to repeat yourself.
         --
@@ -37,50 +38,70 @@ return { -- LSP Configuration & Plugins
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+        if client and client.server_capabilities.definitionProvider then
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        end
 
         -- Find references for the word under your cursor.
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        if client and client.server_capabilities.referencesProvider then
+          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        end
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        if client and client.server_capabilities.implementationProvider then
+          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        end
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        if client and client.server_capabilities.typeDefinitionProvider then
+          map('<leader>lD', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        end
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map('<leader>ld', require('telescope.builtin').lsp_document_symbols, '[D]ocument Symbols')
+        if client and client.server_capabilities.workspaceSymbolProvider then
+          map('<leader>ld', require('telescope.builtin').lsp_document_symbols, '[D]ocument Symbols')
+        end
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        map('<leader>lw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace Symbols')
+        if client and client.server_capabilities.workspaceSymbolProvider then
+          map('<leader>lw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace Symbols')
+        end
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
-        map('<leader>lr', vim.lsp.buf.rename, '[R]ename')
+        if client and client.server_capabilities.renameProvider then
+          map('<leader>lr', vim.lsp.buf.rename, '[R]ename')
+        end
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('<leader>lc', vim.lsp.buf.code_action, '[C]ode Action')
+        if client and client.server_capabilities.codeActionProvider then
+          map('<leader>lc', vim.lsp.buf.code_action, '[C]ode Action')
+        end
 
         -- Opens a popup that displays documentation about the word under your cursor
         --  See `:help K` for why this keymap.
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        if client and client.server_capabilities.hoverProvider then
+          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        end
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        if client and client.server_capabilities.declarationProvider then
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        end
 
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
         --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.server_capabilities.documentHighlightProvider then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
@@ -147,6 +168,8 @@ return { -- LSP Configuration & Plugins
       --
       deno = {},
       html = {},
+      ltex = {},
+      -- lemminx = {},
       lua_ls = {
         -- cmd = {...},
         -- filetypes = { ...},
