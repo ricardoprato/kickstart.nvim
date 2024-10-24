8b1bf1526f2163465127d597c209b353961067fa
ricardoprato <ricardoprato36@gmail.com>
2024-10-24 08:11:52 -0300

    ⌨️ [ADD] new keybindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-local function map(mode, lhs, rhs, args)
-  vim.keymap.set(mode, lhs, rhs, args)
-end
+local kitty = require 'utils.kitty'
+local map = require('utils').map
 
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
@@ -17,10 +16,30 @@ end, { desc = 'Go to next [D]iagnostic message' })
 map('n', '<leader>le', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 map('n', '<leader>lq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+if vim.env.KITTY_WINDOW_ID then
+  map('n', '<C-h>', function()
+    kitty.navigateLeft()
+  end, { desc = 'Move focus to the left split' })
+  map('n', '<C-j>', function()
+    kitty.navigateDown()
+  end, { desc = 'Move focus to the lower split' })
+  map('n', '<C-k>', function()
+    kitty.navigateUp()
+  end, { desc = 'Move focus to the upper split' })
+  map('n', '<C-l>', function()
+    kitty.navigateRight()
+  end, { desc = 'Move focus to the right split' })
+else
+  map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+  map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+  map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+  map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+end
+
+map('n', '<C-Up>', '<cmd>resize -2<CR>', { desc = 'Resize split up' })
+map('n', '<C-Down>', '<cmd>resize +2<CR>', { desc = 'Resize split down' })
+map('n', '<C-Left>', '<cmd>vertical resize -2<CR>', { desc = 'Resize split left' })
+map('n', '<C-Right>', '<cmd>vertical resize +2<CR>', { desc = 'Resize split right' })
 
 -- Move current line / block with Alt-j/k ala vscode.
 map('n', '<A-k>', '<cmd>m .-2<CR>')
