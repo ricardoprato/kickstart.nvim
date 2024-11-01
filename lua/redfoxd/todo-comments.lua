9657bccbe4d70211a721db450e34f8209fe03fb3
ricardoprato <ricardoprato36@gmail.com>
2024-11-01 09:08:42 -0300

    ✨ [ADD] To-do searching

--- lua/redfoxd/todo-comments.lua
+++ lua/redfoxd/todo-comments.lua
@@ -1,1 +1,11 @@
-return { 'folke/todo-comments.nvim', event = 'VimEnter', opts = { signs = false } }
+return {
+  'folke/todo-comments.nvim',
+  config = function()
+    require('todo-comments').setup { signs = false }
+    local function map(mode, lhs, rhs, args)
+      vim.keymap.set(mode, lhs, rhs, args)
+    end
+
+    map('n', '<leader>st', ':TodoTelescope<CR>', { desc = '[S]earch [T]odos' })
+  end,
+}
