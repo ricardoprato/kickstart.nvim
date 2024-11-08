b0392272c0a50d285db73f972be8821ab9d8c6b2
ricardoprato <ricardoprato36@gmail.com>
2024-11-07 21:44:50 -0300

    💄 [UPD] better UI for notifications

--- lua/redfoxd/noice.lua
+++ lua/redfoxd/noice.lua
@@ -25,6 +25,6 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    { 'rcarriga/nvim-notify', opts = {render = 'minimal', top_down = false } },
+    { 'rcarriga/nvim-notify', opts = { render = 'wrapped-compact', top_down = false } },
   },
 }
