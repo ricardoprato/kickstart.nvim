e22fd3d1023989f99d6fddcf529d00d28544b466
ricardoprato <ricardoprato36@gmail.com>
2024-11-07 21:46:10 -0300

    🔨 [ADD] new autocommands

--- lua/autocommand.lua
+++ lua/autocommand.lua
@@ -10,8 +10,34 @@ local autocmd = vim.api.nvim_create_autocmd
 --  See `:help vim.highlight.on_yank()`
 autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = augroup('kickstart-highlight-yank'),
+  group = augroup 'kickstart-highlight-yank',
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Check if we need to reload the file when it changed
+autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
+  group = augroup 'checktime',
+  callback = function()
+    if vim.o.buftype ~= 'nofile' then
+      vim.cmd 'checktime'
+    end
+  end,
+})
+
+autocmd('BufWinEnter', {
+  desc = 'Make q close help, man, quickfix, dap floats',
+  group = augroup 'q_close_windows',
+  callback = function(args)
+    local buftype = vim.api.nvim_get_option_value('buftype', { buf = args.buf })
+    if vim.tbl_contains({ 'help', 'nofile', 'quickfix' }, buftype) and vim.fn.maparg('q', 'n') == '' then
+      vim.keymap.set('n', 'q', '<cmd>close<cr>', {
+        desc = 'Close window',
+        buffer = args.buf,
+        silent = true,
+        nowait = true,
+      })
+    end
   end,
 })
