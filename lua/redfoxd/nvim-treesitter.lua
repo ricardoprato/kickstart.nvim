b8b58c534dc6b08286c4d1094922ed198802f1a0
ricardoprato <ricardoprato36@gmail.com>
2025-05-17 19:48:57 -0300

    🐛 Disable aditional regex

--- lua/redfoxd/nvim-treesitter.lua
+++ lua/redfoxd/nvim-treesitter.lua
@@ -12,7 +12,7 @@ return { -- Highlight, edit, and navigate code
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby', 'xml' },
+      -- additional_vim_regex_highlighting = false,
     },
     indent = { enable = true, disable = { 'ruby', 'xml' } },
   },
