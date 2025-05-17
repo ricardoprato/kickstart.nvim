bd9b956fb8a179151cae2094b6857bf0457ee96c
ricardoprato <ricardoprato36@gmail.com>
2025-05-17 19:46:38 -0300

    🐛 Replace deprecated highlight function

--- lua/autocommand.lua
+++ lua/autocommand.lua
@@ -7,12 +7,12 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+--  See `:help vim.hl.on_yank()`
 autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = augroup 'kickstart-highlight-yank',
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
