6cf272219538db2d8c3e9054d4edf61cadde5ae4
ricardoprato <ricardoprato36@gmail.com>
2025-07-16 17:39:23 -0300

    ⬆️ [UPD] colorscheme

--- lua/redfoxd/colorscheme.lua
+++ lua/redfoxd/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
   { -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
-    -- lazy = false,
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- name = 'catppuccin',
-    -- init = function()
-    --   vim.cmd.colorscheme 'catppuccin'
-    -- end,
+    lazy = false,
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    name = 'catppuccin',
+    init = function()
+      vim.cmd.colorscheme 'catppuccin'
+    end,
   },
   {
     'rose-pine/neovim',
@@ -24,11 +24,11 @@ return {
   },
   { -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    name = 'tokyonight',
-    init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
+    -- lazy = false,
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- name = 'tokyonight',
+    -- init = function()
+    --   vim.cmd.colorscheme 'tokyonight-night'
+    -- end,
   },
 }
