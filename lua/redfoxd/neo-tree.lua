8dd62975cc83fc0ced59c4b21e851179bca6ea9b
Ricardo Prato <ricardoprato36@gmail.com>
2025-12-06 10:59:18 -0300

    [UPD] discomenting neo-tree

--- lua/redfoxd/neo-tree.lua
+++ lua/redfoxd/neo-tree.lua
@@ -1,92 +1,79 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
-return {}
--- return {
---   'nvim-neo-tree/neo-tree.nvim',
---   version = '*',
---   -- dependencies = {
---   --   {
---   --     '3rd/image.nvim',
---   --     lazy = true,
---   --     dependencies = {
---   --       'leafo/magick',
---   --       {
---   --         'vhyrro/luarocks.nvim',
---   --         opts = {
---   --           rocks = {
---   --             hererocks = true,
---   --           },
---   --         },
---   --       },
---   --     },
---   --     opts = {},
---   --   },
---   -- },
---   cmd = 'Neotree',
---   keys = {
---     { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Toggle Neo-tree' },
---   },
---   opts = {
---     commands = {
---       copy_selector = function(state)
---         local node = state.tree:get_node()
---         local filepath = node:get_id()
---         local filename = node.name
---         local modify = vim.fn.fnamemodify
---
---         local vals = {
---           ['BASENAME'] = modify(filename, ':r'),
---           ['EXTENSION'] = modify(filename, ':e'),
---           ['FILENAME'] = filename,
---           ['PATH (CWD)'] = modify(filepath, ':.'),
---           ['PATH (HOME)'] = modify(filepath, ':~'),
---           ['PATH'] = filepath,
---           ['URI'] = vim.uri_from_fname(filepath),
---         }
---
---         local options = vim.tbl_filter(function(val)
---           return vals[val] ~= ''
---         end, vim.tbl_keys(vals))
---         if vim.tbl_isempty(options) then
---           return
---         end
---         table.sort(options)
---         vim.ui.select(options, {
---           prompt = 'Choose to copy to clipboard:',
---           format_item = function(item)
---             return ('%s: %s'):format(item, vals[item])
---           end,
---         }, function(choice)
---           local result = vals[choice]
---           if result then
---             vim.fn.setreg('+', result)
---           end
---         end)
---       end,
---       find_in_dir = function(state)
---         local node = state.tree:get_node()
---         local path = node:get_id()
---         require('telescope.builtin').live_grep {
---           cwd = node.type == 'directory' and path or vim.fn.fnamemodify(path, ':h'),
---         }
---       end,
---     }, -- A list of functions
---     filesystem = {
---       window = {
---         mappings = {
---           ['l'] = 'open',
---           ['h'] = 'close_node',
---           ['\\'] = 'close_window',
---           ['Y'] = 'copy_selector',
---           ['F'] = 'find_in_dir',
---         },
---       },
---       follow_current_file = {
---         enabled = true, -- This will find and focus the file in the active buffer every time
---       },
---       use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
---       -- instead of relying on nvim autocmd events.
---     },
---   },
--- }
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  -- REFACTOR: Enabled the plugin and configured it to lazy-load on command or keypress.
+  cmd = 'Neotree',
+  keys = {
+    { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Toggle Neo-tree' },
+  },
+  opts = {
+    commands = {
+      copy_selector = function(state)
+        local node = state.tree:get_node()
+        local filepath = node:get_id()
+        local filename = node.name
+        local modify = vim.fn.fnamemodify
+
+        local vals = {
+          ['BASENAME'] = modify(filename, ':r'),
+          ['EXTENSION'] = modify(filename, ':e'),
+          ['FILENAME'] = filename,
+          ['PATH (CWD)'] = modify(filepath, ':.'),
+          ['PATH (HOME)'] = modify(filepath, ':~'),
+          ['PATH'] = filepath,
+          ['URI'] = vim.uri_from_fname(filepath),
+        }
+
+        local options = vim.tbl_filter(function(val)
+          return vals[val] ~= ''
+        end, vim.tbl_keys(vals))
+        if vim.tbl_isempty(options) then
+          return
+        end
+        table.sort(options)
+        vim.ui.select(options, {
+          prompt = 'Choose to copy to clipboard:',
+          format_item = function(item)
+            return ('%s: %s'):format(item, vals[item])
+          end,
+        }, function(choice)
+          local result = vals[choice]
+          if result then
+            vim.fn.setreg('+', result)
+          end
+        end)
+      end,
+      find_in_dir = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require('telescope.builtin').live_grep {
+          cwd = node.type == 'directory' and path or vim.fn.fnamemodify(path, ':h'),
+        }
+      end,
+    }, -- A list of functions
+    filesystem = {
+      window = {
+        mappings = {
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+          ['\\'] = 'close_window',
+          ['Y'] = 'copy_selector',
+          ['F'] = 'find_in_dir',
+        },
+      },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+      },
+      use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+      -- instead of relying on nvim autocmd events.
+    },
+  },
+}
